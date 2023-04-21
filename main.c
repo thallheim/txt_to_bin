3d7bd3b714ba58c0bb9eeba690281ad4025106c5
thallheim <91142701+thallheim@users.noreply.github.com>
2023-04-21 18:15:37 +0200

    added error strings for input file, added output confirmation

--- main.c
+++ main.c
@@ -15,22 +15,28 @@ int main(int argc, char *argv[]) {
     FILE *text_file, *binary_file;
     char ch;
 
-    text_file = fopen(argv[1], "r");
-    if (text_file == NULL) {
-      printf("ERROR: Failed to open input file: %s", argv[1]);
-    }
-        
-    binary_file = fopen("OUTPUT.bin", "wb");
-    if (binary_file == NULL) {
-      printf("ERROR: Failed to write output binary.");
-    }
- 
-    while ((ch = fgetc(text_file)) != EOF) {
-      fwrite(&ch, sizeof(char), 1, binary_file);
-    }
+    if (!argv[1]) {
+      printf("ERROR: No input file provided. Exiting.");
+    } else {
 
-    fclose(text_file);
-    fclose(binary_file);
+      text_file = fopen(argv[1], "r");
+      if (text_file == NULL) {
+	printf("ERROR: Failed to open input file: %s", argv[1]);
+      }
+        
+      binary_file = fopen("OUTPUT.bin", "wb");
+      if (binary_file == NULL) {
+	printf("ERROR: Failed to write output binary.");
+      }
  
+      while ((ch = fgetc(text_file)) != EOF) {
+	fwrite(&ch, sizeof(char), 1, binary_file);
+      }
+
+      fclose(text_file);
+      fclose(binary_file);
+      printf("Wrote file: OUTPUT.bin");
+    }
+    
     return 0;
 }
